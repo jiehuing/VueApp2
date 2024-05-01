90d4eba2507e3c8396e29223af8190bc698d0739
jiehuing <ngjh94@gmail.com>
2024-05-01 16:19:52 +0800

    import css

--- vueapp2.client/src/main.ts
+++ vueapp2.client/src/main.ts
@@ -1,6 +1,9 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config';
 import App from './App.vue'
+import 'primevue/resources/primevue.min.css';
+import 'primevue/resources/themes/aura-light-green/theme.css';
 
-createApp(App).mount('#app')
+createApp(App).use(PrimeVue).mount('#app');
